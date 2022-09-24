a1ce9fab4f6065bf1e4a7568ec25f4c91a06f900
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-09-24 04:08:12 +0000

    [Add]Javaの練習

---
+++ Sample1.java
@@ -0,0 +1,7 @@
+public class Sample1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int number = 10;
+        System.out.println(number);
+    }
+}
