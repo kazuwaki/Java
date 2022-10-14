7a458626830d13adf9d93f57e52f86fe077e4eea
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-10-14 13:59:51 +0000

    [Add]コメント追記

--- Clac.java
+++ Clac.java
@@ -3,10 +3,10 @@ public class Clac {
     int a = 10; int b = 2;
     int total = ClacLogic.tasu(a, b);
     int delta = ClacLogic.hiku(a, b);
-    // int total = tasu(a,b);
-    // int delta = hiku(a,b);
-    //6,7行目でtasu()やhiku()を呼び出しているがメソッドがないのでエラーが起きる
-    //mainから呼び出すにはClacLogicのtasu()とClacLogicのhiku()と明確に所属を示す（4,5行目）
+    /* int total = tasu(a,b);
+    int delta = hiku(a,b);
+    6,7行目でtasu()やhiku()を呼び出しているがメソッドがないのでエラーが起きる
+    mainから呼び出すにはClacLogicのtasu()とClacLogicのhiku()と明確に所属を示す（4,5行目）*/
     System.out.println("足すと" + total + "、引くと" + delta);
   }
 }
