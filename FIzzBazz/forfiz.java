ab20e1c4f8185a43654bbf3c865325baa3b8296a
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-10-20 08:33:28 +0000

    [Add]for文のFizzBuzz

---
+++ FIzzBazz/forfiz.java
@@ -0,0 +1,16 @@
+public class forfiz {
+  public static void main(String[] args) {
+    int i;
+    for(i = 1; i < 31; i++){
+      if (i % 15 == 0){
+        System.out.println("FizzBuzz");
+      } else if (i % 5 == 0){
+        System.out.println("Fizz");
+      } else if (i % 3 == 0){
+        System.out.println("Buzz");
+      } else {
+        System.out.print(i);
+      }
+    }
+  }
+}
