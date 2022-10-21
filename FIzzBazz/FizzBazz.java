32468a28e1ab588fe0fda02313be97d96cf0844b
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-10-21 14:50:10 +0000

    [Add]コメント記入

--- FIzzBazz/FizzBazz.java
+++ FIzzBazz/FizzBazz.java
@@ -1,7 +1,9 @@
 public class FizzBazz {
   public static void main(String[] args) {
     System.out.println("好きな数字を入力してください");
+    //nextIntは数値の入力を受け取る
     int num = new java.util.Scanner(System.in).nextInt();
+    //１５から割り算しないと5や3が先に引っかかるからだめ
     if (num % 15 == 0){
      System.out.println("FizzBazz"); 
     } else if (num % 5 == 0) {
